518f4403521b6e8ed92e6180f118ddd44a98f354
hades4you <murphysurya@gmail.com>
2024-03-20 12:01:39 +0530

    app built

--- src/index.js
+++ src/index.js
@@ -1,42 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./css/index.css";
 
 const Books = [
   {
     image: "./images/meditations.jpg",
     title: "Meditations",
     author: "Marcus Aureilus",
+    id: 1,
   },
   {
     image: "./images/lawsOfPower.jpg",
     title: "48 Laws Of Power",
     author: "Robert Greene",
+    id: 2,
   },
   {
     image: "./images/theRichestManInBabylon.jpg",
     title: "The Richest Man In Babylon",
     author: "George S. Clason",
+    id: 3,
+  },
+  {
+    image: "./images/atomicHabits.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+    id: 4,
   },
 ];
 
-const Booklist = () =>
-  Books.map((element) => (
-    <>
-      <Book
-        image={element.image}
-        title={element.title}
-        author={element.author}
-      />
-    </>
-  ));
+const Booklist = () => (
+  <>
+    <p id="thetitle">My Book List</p>
+    <EventTesting />
+    <div className="booklist">
+      {Books.map((element) => {
+        return <Book element={element} key={element.id} />;
+      })}
+    </div>
+  </>
+);
 
 const Book = (props) => {
-  const { image, title, author } = props;
+  const { image, title, author } = props.element;
+  return (
+    <div className="book">
+      <img src={image} alt={title} />
+      <p className="title">{title}</p>
+      <p className="author">{author}</p>
+    </div>
+  );
+};
+
+const EventTesting = () => {
+  const fieldEvent = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+  };
+
+  const buttonEvent = () => alert("Button Clicked");
+
+  const formEvent = (e) => e.preventDefault;
+  console.log("form submitted");
+
   return (
     <>
-      <img src={image} alt={title} />
-      <p>{title}</p>
-      <p>{author}</p>
+      <form onSubmit={formEvent}>
+        <input type="text" name="textfield" onChange={fieldEvent} />
+      </form>
+      <button onClick={buttonEvent}>I'm a button</button>
     </>
   );
 };
